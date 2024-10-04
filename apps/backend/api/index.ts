41843b52f16b93af0fc17c1d4441f6c42d3a189d
feyzi <mfeyziodabas@gmail.com>
2024-10-04 23:39:10 +0200

    Added Vercel.json

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,16 +1,28 @@
-import { ApolloServer } from 'apollo-server';
-import { PrismaClient } from '@prisma/client';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+import express from "express";
+import cors from "cors";
+
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
-const prisma = new PrismaClient();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ prisma, req }),
-});
+const startApolloServer = async(app: any, httpServer: any) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
